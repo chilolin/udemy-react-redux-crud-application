18a5d85e4398638de9461687188c40d619b5ca54
chilolin <yuma25173@gamil.com>
2020-08-30 22:57:44 +0900

    implement action and action creater

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-export const App = () => {
-  const profiles = [
-    {name: "Taro", age: 10 },
-    {name: "Hanako", age: 5 },
-    {name: "NoName", age: 3 },
-  ]
-  return (
-    <div>
-      {
-        profiles.map((profile, index) => {
-          return <User name={profile.name} age={profile.age} key={index} />
-        })
-      }
-    </div>
-  );  
-}
+export const App = () => (<Counter></Counter>)
 
-const User = (props) => {
-  return <div>Hi, I am {props.name}, and {props.age} years old!</div>;
-}
-
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired,
+class Counter extends React.Component {
+  constructor(props) {
+    super(props)
+    console.log(this.state)
+    this.state = { count: 0 }
+  }
+  render() {
+    return (
+      <div>count: {this.state.count}</div>
+    )
+  }
 }
